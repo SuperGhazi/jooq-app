b6e7b0f47799f9811152ac4df2f3cc1de07609e3
Ghazi Hakim <ghazi.hakim@alenia.io>
2025-06-10 15:39:52 +0200

    init project

---
+++ src/test/java/dev/ghazi/app/JooqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ghazi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
